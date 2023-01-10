143331339aaa89ae34ea48dea09ff5b346fba267
VictorBuch <victorbuch@protonmail.com>
2023-01-10 23:33:58 +0100

    add capacitor preference

--- svelte/src/routes/+layout.svelte
+++ svelte/src/routes/+layout.svelte
@@ -1,22 +1,41 @@
 <script>
 	import './styles.css';
-  import { supabaseClient } from '$lib/db'
-  import { invalidate } from '$app/navigation'
-  import { onMount } from 'svelte'
+	import { supabaseClient } from '$lib/db';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
 
-  onMount(() => {
-    const {
-      data: { subscription },
-    } = supabaseClient.auth.onAuthStateChange(() => {
-      invalidate('supabase:auth')
-    })
+	// TODO: make storing user work
+	// import { Preferences } from '@capacitor/preferences';
 
-    return () => {
-      subscription.unsubscribe()
-    }
-  })
+	// // JSON "set" example
+	// async setObject() {
+	//   await Preferences.set({
+	//     key: 'user',
+	//     value: JSON.stringify({
+	//       id: 1,
+	//       name: 'Max'
+	//     })
+	//   });
+	// }
+
+	// // JSON "get" example
+	// async getObject() {
+	//   const ret = await Preferences.get({ key: 'user' });
+	//   const user = JSON.parse(ret.value);
+	// }
+
+	onMount(async () => {
+		const {
+			data: { subscription }
+		} = supabaseClient.auth.onAuthStateChange(() => {
+			invalidate('supabase:auth');
+		});
+		return () => {
+			subscription.unsubscribe();
+		};
+	});
 </script>
 
 <div class="app overflow-x-hidden h-screen">
-		<slot />
+	<slot />
 </div>
